d60957e4c45bb8bef0a02e8f39b1fa482d2d51b7
renat <murzinarenata@gmail.com>
2024-11-09 16:53:29 -0800

    feat: artists implement

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 ## Downloading
 
 ```
-git clone {repository URL}
+git clone https://github.com/RenaSpb/nodejs2024Q3-service
 ```
 
 ## Installing NPM modules
---
+++ src/artists/artists.service.ts
@@ -0,0 +1,88 @@
+import {
+  Injectable,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
+import { validate as validateUUID, v4 as uuidv4 } from 'uuid';
+import { Artist } from './entities/artist.entity';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+
+@Injectable()
+export class ArtistsService {
+  private artists: Artist[] = [];
+
+  findAll(): Artist[] {
+    return this.artists;
+  }
+
+  findById(id: string): Artist {
+    if (!validateUUID(id)) {
+      throw new BadRequestException('Invalid artist id');
+    }
+    const artist = this.artists.find((artist) => artist.id === id);
+    if (!artist) {
+      throw new BadRequestException('Artist not found');
+    }
+    return artist;
+  }
+
+  create(createArtistDto: CreateArtistDto) {
+    if (
+      typeof createArtistDto.name !== 'string' ||
+      typeof createArtistDto.grammy !== 'boolean'
+    ) {
+      throw new BadRequestException('Body does not contain required fields');
+    }
+
+    const newArtist: Artist = {
+      id: uuidv4(),
+      name: createArtistDto.name,
+      grammy: createArtistDto.grammy,
+    };
+
+    this.artists.push(newArtist);
+    return newArtist;
+  }
+
+  update(id: string, updateArtistDto: UpdateArtistDto): Artist {
+    if (!validateUUID(id)) {
+      throw new BadRequestException('Invalid artist id');
+    }
+
+    if (
+      typeof updateArtistDto.name !== 'string' ||
+      typeof updateArtistDto.grammy !== 'boolean'
+    ) {
+      throw new BadRequestException('Invalid data type');
+    }
+
+    const artistIndex = this.artists.findIndex((artist) => artist.id === id);
+    if (artistIndex === -1) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    this.artists[artistIndex] = {
+      ...this.artists[artistIndex],
+      ...updateArtistDto,
+    };
+
+    return this.artists[artistIndex];
+  }
+
+  remove(id: string): void {
+    if (!validateUUID(id)) {
+      throw new BadRequestException('Invalid artist id');
+    }
+
+    const artistIndex = this.artists.findIndex((artists) => artists.id === id);
+    if (artistIndex === -1) {
+      throw new NotFoundException('Artist not found');
+    }
+
+    this.artists.splice(artistIndex, 1);
+    // TODO: При имплементации tracks и albums:
+    // 1. Найти все треки с artistId === id и установить их artistId в null
+    // 2. Найти все альбомы с artistId === id и установить их artistId в null
+  }
+}
