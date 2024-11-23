deb024d52ce168c93bca6d1b7fc044991c31cbc6
renat <murzinarenata@gmail.com>
2024-11-22 22:43:37 -0800

    feat: auth fixes

--- src/albums/albums.module.ts
+++ src/albums/albums.module.ts
@@ -1,4 +1,3 @@
-// src/albums/albums.module.ts
 import { Module, forwardRef } from '@nestjs/common';
 import { AlbumsController } from './albums.controller';
 import { AlbumsService } from './albums.service';
